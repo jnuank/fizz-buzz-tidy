828d2934b09aba4f2c7600a0ffeeb7fb89b63fc4
jnuank <ikuta1919@gmail.com>
2025-01-15 21:34:38 +0900

    S

--- src/test/kotlin/FizzBuzzTest.kt
+++ src/test/kotlin/FizzBuzzTest.kt
@@ -13,4 +13,9 @@ class FizzBuzzTest {
     fun 数字2を渡したら文字列の2を返す() {
         assertEquals("2", FizzBuzz.fizzBuzz(2))
     }
+
+    @Test
+    fun 数字3を渡したら文字列のFizzを返す() {
+        assertEquals("Fizz", FizzBuzz.fizzBuzz(3))
+    }
 }
