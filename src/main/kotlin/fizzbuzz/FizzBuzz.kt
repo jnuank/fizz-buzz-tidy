35a81a4b14ac4e4adb023df1350c4ddf7b0b80a1
jnuank <ikuta1919@gmail.com>
2025-01-15 21:35:03 +0900

    B

--- src/main/kotlin/fizzbuzz/FizzBuzz.kt
+++ src/main/kotlin/fizzbuzz/FizzBuzz.kt
@@ -3,6 +3,9 @@ package org.example.fizzbuzz
 class FizzBuzz {
     companion object {
         fun fizzBuzz(number: Int): String {
+            if (number == 3) {
+                return "Fizz"
+            }
             return number.toString()
         }
     }
