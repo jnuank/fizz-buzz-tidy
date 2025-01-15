736299968e315878f8b3dc9696d627c9f2d93940
jnuank <ikuta1919@gmail.com>
2025-01-15 21:30:19 +0900

    S

--- src/test/kotlin/FizzBuzzTest.kt
+++ src/test/kotlin/FizzBuzzTest.kt
@@ -5,11 +5,6 @@ import kotlin.test.assertEquals
 class FizzBuzzTest {
 
     @Test
-    fun 動くかな() {
-        assertEquals(2, 2)
-    }
-
-    @Test
     fun 数字1を渡したら文字列の1を返す() {
         assertEquals("1", FizzBuzz.fizzBuzz(1))
     }
