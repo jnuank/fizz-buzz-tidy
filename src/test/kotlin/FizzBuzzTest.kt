7409d10a5feda786217789296a7376a28310d822
jnuank <ikuta1919@gmail.com>
2025-01-15 21:31:29 +0900

    S

--- src/test/kotlin/FizzBuzzTest.kt
+++ src/test/kotlin/FizzBuzzTest.kt
@@ -8,4 +8,9 @@ class FizzBuzzTest {
     fun 数字1を渡したら文字列の1を返す() {
         assertEquals("1", FizzBuzz.fizzBuzz(1))
     }
+
+    @Test
+    fun 数字2を渡したら文字列の2を返す() {
+        assertEquals("2", FizzBuzz.fizzBuzz(2))
+    }
 }
