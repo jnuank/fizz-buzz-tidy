323a273bd859c7c365ff04a750123a9a0468cf42
jnuank <ikuta1919@gmail.com>
2025-01-15 22:02:32 +0900

    S

--- src/main/kotlin/fizzbuzz/FizzBuzz.kt
+++ src/main/kotlin/fizzbuzz/FizzBuzz.kt
@@ -4,13 +4,15 @@ class FizzBuzz {
     companion object {
         fun fizzBuzz(number: Int): String =
             when {
-                number isDivisibleBy 3  && number isDivisibleBy 5  -> "FizzBuzz"
+                number isDivisibleBy (3 to 5)  -> "FizzBuzz"
                 number isDivisibleBy 3 -> "Fizz"
                 number isDivisibleBy 5  -> "Buzz"
                 else -> number.toString()
             }
 
         private infix fun Int.isDivisibleBy(divisor: Int) = this % divisor == 0
+        private infix fun Int.isDivisibleBy(divisors: Pair<Int, Int>) = this % divisors.first == 0 && this % divisors.second == 0
+
     }
 
 }
