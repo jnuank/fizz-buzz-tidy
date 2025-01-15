bf3ffc35653b14e5bd1607f703c277c3a542a453
jnuank <ikuta1919@gmail.com>
2025-01-15 21:51:59 +0900

    S

--- src/main/kotlin/fizzbuzz/FizzBuzz.kt
+++ src/main/kotlin/fizzbuzz/FizzBuzz.kt
@@ -3,18 +3,14 @@ package org.example.fizzbuzz
 class FizzBuzz {
     companion object {
         fun fizzBuzz(number: Int): String =
-            if (isThreeDivisible(number) && isFiveDivisible(number)) {
-                "FizzBuzz"
-            } else if (isThreeDivisible(number)) {
-                "Fizz"
-            } else if (isFiveDivisible(number)) {
-                "Buzz"
-            } else {
-                number.toString()
+            when {
+                isThreeDivisible(number) && isFiveDivisible(number) -> "FizzBuzz"
+                isThreeDivisible(number) -> "Fizz"
+                isFiveDivisible(number) -> "Buzz"
+                else -> number.toString()
             }
 
         private fun isFiveDivisible(number: Int) = number % 5 == 0
-
         private fun isThreeDivisible(number: Int) = number % 3 == 0
     }
 
